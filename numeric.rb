218c4df937d2c796a705b488e02ff3cdcac07d8c
myokoym <myokoym@gmail.com>
2010-09-18 14:25:08 +0900

    rename to Numeric#to_en

--- numeric.rb
+++ numeric.rb
@@ -1,5 +1,5 @@
 class Numeric
-  def to_w
+  def to_en
     return self.to_s unless self.is_a?(Integer)
     case self
     when 1
@@ -59,46 +59,46 @@ class Numeric
     when 21 .. 99
       x_one = self % 10
       x_ten = self - x_one
-      x_ten.to_w + "-" + x_one.to_w
+      x_ten.to_en + "-" + x_one.to_en
     when 100 .. 999
       front_num = self / 100
       rear_num = self % 100
       if rear_num == 0
-        front_num.to_w + " hundred"
+        front_num.to_en + " hundred"
       else
-        front_num.to_w + " hundred and " + rear_num.to_w
+        front_num.to_en + " hundred and " + rear_num.to_en
       end
     when 1e3 .. 999999
       front_num = self / 1000
       rear_num = self % 1000
       if rear_num == 0
-        front_num.to_w + " thousand"
+        front_num.to_en + " thousand"
       else
-        front_num.to_w + " thousand and " + rear_num.to_w
+        front_num.to_en + " thousand and " + rear_num.to_en
       end
     when 1e6 .. 999999999
       front_num = self / 1000000
       rear_num = self % 1000000
       if rear_num == 0
-        front_num.to_w + " million"
+        front_num.to_en + " million"
       else
-        front_num.to_w + " million and " + rear_num.to_w
+        front_num.to_en + " million and " + rear_num.to_en
       end
     when 1e9 .. 999999999999
       front_num = self / 1000000000
       rear_num = self % 1000000000
       if rear_num == 0
-        front_num.to_w + " billion"
+        front_num.to_en + " billion"
       else
-        front_num.to_w + " billion and " + rear_num.to_w
+        front_num.to_en + " billion and " + rear_num.to_en
       end
     when 1e12 .. 999999999999999
       front_num = self / 1000000000000
       rear_num = self % 1000000000000
       if rear_num == 0
-        front_num.to_w + " trillion"
+        front_num.to_en + " trillion"
       else
-        front_num.to_w + " trillion and " + rear_num.to_w
+        front_num.to_en + " trillion and " + rear_num.to_en
       end
     else
       self.to_s
--- spec/numeric_spec.rb
+++ spec/numeric_spec.rb
@@ -1,80 +1,80 @@
 require "numeric"
 
-describe Numeric, "#to_w" do
+describe Numeric, "#to_en" do
   it "1 to one" do
-    1.to_w.should == "one"
+    1.to_en.should == "one"
   end
 
   it "12 to twelve" do
-    12.to_w.should == "twelve"
+    12.to_en.should == "twelve"
   end
 
   it "13 to thirteen" do
-    13.to_w.should == "thirteen"
+    13.to_en.should == "thirteen"
   end
 
   it "20 to twenty" do
-    20.to_w.should == "twenty"
+    20.to_en.should == "twenty"
   end
 
   it "24 to twenty-four" do
-    24.to_w.should == "twenty-four"
+    24.to_en.should == "twenty-four"
   end
 
   it "100 to one hundred" do
-    100.to_w.should == "one hundred"
+    100.to_en.should == "one hundred"
   end
 
   it "101 to one hundred and one" do
-    101.to_w.should == "one hundred and one"
+    101.to_en.should == "one hundred and one"
   end
   
   it "115 to one hundred and fifteen" do
-    115.to_w.should == "one hundred and fifteen"
+    115.to_en.should == "one hundred and fifteen"
   end
 
   it "342 to three hundred and forty-two" do
-    342.to_w.should == "three hundred and forty-two"
+    342.to_en.should == "three hundred and forty-two"
   end
 
   it "500 to five hundred" do
-    500.to_w.should == "five hundred"
+    500.to_en.should == "five hundred"
   end
 
   it "1000 to one thousand" do
-    1000.to_w.should == "one thousand"
+    1000.to_en.should == "one thousand"
   end
 
   it "1009 to one thousand and nine" do
-    1009.to_w.should == "one thousand and nine"
+    1009.to_en.should == "one thousand and nine"
   end
 
   it "1234 to one thousand and two hundred and thirty-four" do
-    1234.to_w.should == "one thousand and two hundred and thirty-four"
+    1234.to_en.should == "one thousand and two hundred and thirty-four"
   end
 
   it "99000 to ninety-nine thousand" do
-    99000.to_w.should == "ninety-nine thousand"
+    99000.to_en.should == "ninety-nine thousand"
   end
 
   it "1000000 to one million" do
-    1000000.to_w.should == "one million"
+    1000000.to_en.should == "one million"
   end
 
   it "6000000000 to six billion" do
-    6000000000.to_w.should == "six billion"
+    6000000000.to_en.should == "six billion"
   end
 
   it "71000000000000 to seventy-one trillion" do
-    71000000000000.to_w.should == "seventy-one trillion"
+    71000000000000.to_en.should == "seventy-one trillion"
   end
   
   it "823456789012345 to eight hundred and twenty-three trillion and four hundred and fifty-six billion and seven hundred and eighty-nine million and twelve thousand and three hundred and forty-five" do
-    823456789012345.to_w.should == "eight hundred and twenty-three trillion and four hundred and fifty-six billion and seven hundred and eighty-nine million and twelve thousand and three hundred and forty-five"
+    823456789012345.to_en.should == "eight hundred and twenty-three trillion and four hundred and fifty-six billion and seven hundred and eighty-nine million and twelve thousand and three hundred and forty-five"
   end
 
   it "1.3 to \"1.3\"" do
-    1.3.to_w.should == "1.3"
+    1.3.to_en.should == "1.3"
   end
 end
 
